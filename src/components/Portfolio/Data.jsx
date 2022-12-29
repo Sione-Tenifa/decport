090005a22d6a5637bd5739aff9faf275c88512d8
sione-tenifa <johntenifa@yahoo.com>
2022-12-29 14:58:12 -0800

    edits

---
+++ src/components/Portfolio/Data.jsx
@@ -0,0 +1,56 @@
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.jpg";
+import Todo from "../../assets/todo.png"
+
+export const projectsData = [
+  {
+    id: 1,
+    image: Todo,
+    title: "Web design",
+    category: "web",
+    url: "https://todolist-indol-psi.vercel.app/"
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "App movil",
+    category: "app",
+  },
+  // {
+  //   id: 3,
+  //   image: Work3,
+  //   title: "Brand design",
+  //   category: "design",
+  // },
+  // {
+  //   id: 4,
+  //   image: Work4,
+  //   title: "App movil",
+  //   category: "app",
+  // },
+  // {
+  //   id: 5,
+  //   image: Work5,
+  //   title: "Web design",
+  //   category: "web",
+  // },
+];
+
+// projects
+export const projectsNav = [
+  // {
+  //   name: "all",
+  // },
+  // // {
+  // //   name: "web",
+  // // },
+  // // {
+  // //   name: "app",
+  // // },
+  // // {
+  // //   name: "design",
+  // // },
+];
