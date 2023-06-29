ceb68c0e9e73f8b67f23652be0326f23427e72f5
sione-tenifa <johntenifa@yahoo.com>
2023-06-29 17:42:39 -0600

    added new project

--- src/components/Portfolio/Data.jsx
+++ src/components/Portfolio/Data.jsx
@@ -4,12 +4,13 @@ import water from "../../assets/water.jpeg";
 import Todo from "../../assets/todo.png"
 import Realtor from "../../assets/realtor.png"
 import candy from "../../assets/candycrush.png"
+import password from "../../assets/password.png"
 
 export const projectsData = [
   {
     id: 1,
     image: Todo,
-    title: "React Todo List",
+    title: "React- Todo List",
     category: "web",
     url: "https://todolist-indol-psi.vercel.app/"
   },
@@ -20,29 +21,37 @@ export const projectsData = [
     category: "app",
     url: "https://realestate-api-next.vercel.app/"
   },
-  {
+    {
     id: 3,
+    image: password,
+    title: "React- Password Generator",
+    category: "app",
+    url: "https://passwordgenerator-swart-gamma.vercel.app/"
+  },
+  
+  {
+    id: 4,
     image: Movie,
     title: "Movie API",
     category: "app",
     url: "https://movieapi-kappa.vercel.app/",
   },
   {
-    id: 4,
+    id: 5,
     image: dad,
     title: "Dad Jokes API",
     category: "app",
     url: 'https://dadjokes-liart.vercel.app/', 
   },
   {
-    id: 5,
+    id: 6,
     image: water,
     title: "Daily Water tracker",
     category: "web",
     url: "https://watertracker.vercel.app/",
   },
   {
-    id: 6,
+    id: 7,
     image: candy,
     title: "Candy Crush",
     category: "web",
