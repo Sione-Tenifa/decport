8d790952b105fc47e2228137962fcd10e8ef83f0
Sione Tenifa <johntenifa@yahoo.com>
2026-02-20 16:20:13 -0700

    2026 updates v2

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -8,10 +8,12 @@ const ScrollUp = () => {
     if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
     else scrollUp.classList.remove("show-scroll");
   });
+  const scrollToTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
+
   return (
-    <a href="#" className="scrollup">
+    <button type="button" className="scrollup" onClick={scrollToTop} aria-label="Scroll to top">
       <i className="uil uil-arrow-up scrollup__icon"></i>
-    </a>
+    </button>
   );
 };
 
